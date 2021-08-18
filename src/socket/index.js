bf3a10dff0e368deb9c98359ce7b25f257cc961e
MhShekel <mykhailoshekel@gmail.com>
2021-08-18 08:37:12 +0300

    update: createroom2

--- src/socket/index.js
+++ src/socket/index.js
@@ -12,10 +12,11 @@ const connection = (app, io) => {
         io.to('Player-List').emit('showUsers', users);
         io.to('Player-List').emit('showRooms', rooms);  
       });
-      socket.on('createRoom', ({ roomEmail, createPlayerEmail })=>{
+      socket.on('createRoom', ({ roomEmail, createPlayerEmail }, callback)=>{
+        const {err, room} = addRoom({roomEmail, createPlayerEmail}, callback);
+        if(err) return callback(err);
         socket.join('room: '+ roomEmail);
-        const room = addRoom({roomEmail, createPlayerEmail});
-        io.to("room: "+roomEmail).emit('sameRoomPlayers', room.players);
+        io.to("room: "+roomEmail).emit('sameRoomPlayers', room);
         const rooms = getAllRooms();
         io.to('Player-List').emit('showRooms', rooms);
       })
@@ -25,11 +26,17 @@ const connection = (app, io) => {
         io.to('Player-List').emit('showRooms', rooms);
       })
       socket.on('disconnect', () => {
-        console.log('1111111111111111')
         removeUser(socket.id);
         const users = getAllUsers();
         io.to('Player-List').emit('showUsers', users);
       });
+      socket.on('joinPlayerInRoom', ({roomIndex, playerEmail})=>{
+        const room = addPlayerInRoom({roomIndex, playerEmail});
+        const rooms = getAllRooms();
+        io.to('Player-List').emit('showRooms', rooms);
+        socket.join('room: '+ room.roomEmail);
+        io.to("room: "+room.roomEmail).emit('sameRoomPlayers', room);
+      })
     })    
 }
 
--- src/socket/rooms.js
+++ src/socket/rooms.js
@@ -1,12 +1,13 @@
 const rooms = [];
 
-const addRoom = ({roomEmail, createPlayerEmail}) => {
-
+const addRoom = ({roomEmail, createPlayerEmail}, callback) => {
+  var index = rooms.findIndex((room)=>room.roomEmail === roomEmail)
+  if(index !== -1) return {err: "You have already created room!"}
   const room = { roomEmail, players: [createPlayerEmail]};
 
   rooms.push(room);
 
-  return room ;
+  return {room:room};
 
 }
 
@@ -20,4 +21,18 @@ const getAllRooms = () => {
   return rooms;
 }
 
-module.exports = { addRoom, removeRoom, getAllRooms};
+const addPlayerInRoom = ({roomIndex,playerEmail}) => {
+  var playerRoom;
+  rooms.map((room, index)=>{
+    if(index == roomIndex) {
+      playerRoom = room;
+      var i = room.players.findIndex((email)=> email === playerEmail);
+      if(i === -1) {
+        room.players.push(playerEmail);
+      }
+    }
+  })
+  return playerRoom;
+}
+
+module.exports = { addRoom, removeRoom, getAllRooms, addPlayerInRoom};
